c9d50a77b81157925696883223cffbc31e7e4061
Weffy61 <weffy@mail.ru>
2023-11-05 04:24:59 +0300

    Add inline PlaceImage to admin panel

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title']
+    inlines = [PlaceImageInline]
 
 
 @admin.register(PlaceImage)
