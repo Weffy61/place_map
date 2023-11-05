09e90107633b256292735379ae6139ca19506501
Weffy61 <weffy@mail.ru>
2023-11-06 02:16:10 +0300

    Remove unused import

--- places/views.py
+++ places/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from places.models import Place, PlaceImage
 from django.http import JsonResponse
 
