eba668736e58577c58632f033ab128cbe7911005
Weffy61 <weffy@mail.ru>
2023-11-06 04:00:35 +0300

    Change settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -113,10 +113,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = env.str('STATIC_URL', '/static/')
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = env.str('MEDIA_URL', '/media/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = env.str('STATIC_URL', '/static/')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'asserts')]
+
+
