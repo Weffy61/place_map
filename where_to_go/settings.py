e6f398d2153015ab6f787b755cdb2dc2de24e8dc
Weffy61 <weffy@mail.ru>
2023-11-06 06:16:55 +0300

    Change settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,6 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = env.str('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'places/static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'places'), ]
 
 
