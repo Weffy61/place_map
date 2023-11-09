48f9cb3105b8fddb450e3295129cca4becd117e6
Weffy61 <weffy@mail.ru>
2023-11-10 00:43:42 +0300

    Moved views from project to the app

--- places/views.py
+++ places/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import get_object_or_404
 from places.models import Place, PlaceImage
 from django.http import JsonResponse
+from django.shortcuts import render
+from django.urls import reverse
+
+
+def show_start_page(request):
+    places = Place.objects.all()
+    geojson_feature = []
+    for place in places:
+
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lon, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.pk,
+                "detailsUrl": reverse('places:place_detail', args=[place.pk])
+            }
+        }
+        geojson_feature.append(feature)
+    geojson = {
+        "type": "FeatureCollection",
+        "features": geojson_feature
+    }
+
+    data = {
+        "geojson": geojson
+    }
+    return render(request, 'index.html', context=data)
 
 
 def place_detail(request, place_id):
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import show_start_page
+from places.views import show_start_page
 from django.conf import settings
 from django.conf.urls.static import static
 
