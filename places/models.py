562961a93f4f41ac44fcd524b4557bbd5f435e2d
Weffy61 <weffy@mail.ru>
2023-11-10 01:08:48 +0300

    Remove max length for TextField

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название места')
-    description_short = models.TextField(max_length=250, verbose_name='Краткое описание')
+    description_short = models.TextField(verbose_name='Краткое описание')
     description_long = HTMLField(verbose_name='Полное описание')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
